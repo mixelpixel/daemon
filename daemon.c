a4bdd8858fdb28774f44ea8e4355f74d8988a00b
mixelpixel <mixelpix@mac.com>
2017-10-19 02:25:06 -0400

    starter comment

---
+++ daemon.c
@@ -0,0 +1,6 @@
+/******************************************************************************
+ * A basic system daemon that writes to a log file
+ * per http://www.thegeekstuff.com/2012/02/c-daemon-process/
+ * Author: Patrick Kennedy
+ * Started: October 19, 2017
+ ******************************************************************************/
